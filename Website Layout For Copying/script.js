91f5150fecd6ddbcd54029501b778e75cf165ae7
Nawal Ahmed <nawal.j.ahmed@gmail.com>
2019-06-10 20:36:00 -0400

    almost completely redid the website

---
+++ Website Layout For Copying/script.js
@@ -0,0 +1,43 @@
+/*Foreground (Text) Button Functions*/
+
+function fAfunction(){
+  document.getElementById("box-1").style.color = "#80d6ff";
+}
+
+function fBfunction(){
+  document.getElementById("box-1").style.color = "#c7a4ff";
+}
+
+function fCfunction(){
+  document.getElementById("box-1").style.color = "#b2fab4";
+}
+
+function fDfunction(){
+  document.getElementById("box-1").style.color = "#ffff81";
+}
+
+function fEfunction(){
+  document.getElementById("box-1").style.color = "#ffa4a2";
+  }
+
+/*Background Button Functions*/
+
+function bAfunction(){
+  document.getElementById("box-1").style.backgroundColor = "#1976d2";
+  }
+
+function bBfunction(){
+  document.getElementById("box-1").style.backgroundColor = "#512da8";
+}
+
+function bCfunction(){
+  document.getElementById("box-1").style.backgroundColor = "#388e3c";
+}
+
+function bDfunction(){
+  document.getElementById("box-1").style.backgroundColor = "#e65100";
+}
+
+function bEfunction(){
+  document.getElementById("box-1").style.backgroundColor = "#d32f2f";
+} 
